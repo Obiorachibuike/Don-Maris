bac2e235a15cf825ce3bcdf221ec4773b8ec07df
Chibuike Obiora <obiorachibuike25@gmail.com>
2025-10-05 17:05:25 +0000

    Compiled successfully in 44s 18:03:31.813    Skipping validation of typ

--- next.config.ts
+++ next.config.ts
@@ -31,6 +31,17 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  env: {
+    MONGODB_URI: process.env.MONGODB_URI,
+    JWT_SECRET: process.env.JWT_SECRET,
+    EMAIL_SERVER_USER: process.env.EMAIL_SERVER_USER,
+    EMAIL_SERVER_PASSWORD: process.env.EMAIL_SERVER_PASSWORD,
+    EMAIL_FROM: process.env.EMAIL_FROM,
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+    STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
+    NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
+    PAYSTACK_SECRET_KEY: process.env.PAYSTACK_SECRET_KEY,
+  }
 };
 
 export default nextConfig;
