81a57b2c766902cc34846a9c13c965d19535a753
Chibuike Obiora <obiorachibuike25@gmail.com>
2025-11-20 06:38:28 +0000

    They should also be tab for product to view product and also add and edi

--- src/app/admin/general/page.tsx
+++ src/app/admin/general/page.tsx
@@ -10,35 +10,21 @@ import OrdersPage from "../orders/page";
 
 export default function GeneralAdminPage() {
     return (
-        <Card>
-            <CardHeader>
-                <CardTitle>General Administration</CardTitle>
-                <CardDescription>Manage all aspects of your store from one place.</CardDescription>
-            </CardHeader>
-            <CardContent>
-                <Tabs defaultValue="users" className="w-full">
-                    <TabsList className="grid w-full grid-cols-3">
-                        <TabsTrigger value="users">Users</TabsTrigger>
-                        <TabsTrigger value="products">Products</TabsTrigger>
-                        <TabsTrigger value="orders">Orders</TabsTrigger>
-                    </TabsList>
-                    <TabsContent value="users" className="mt-6">
-                        {/* 
-                            Because the original pages were designed to be full pages, 
-                            their top-level Card component creates nested cards here.
-                            This is a quick way to reuse them. For a production app,
-                            you would refactor the content into components without the Card wrapper.
-                        */}
-                        <UsersPage />
-                    </TabsContent>
-                    <TabsContent value="products" className="mt-6">
-                        <ProductsAdminPage />
-                    </TabsContent>
-                    <TabsContent value="orders" className="mt-6">
-                        <OrdersPage />
-                    </TabsContent>
-                </Tabs>
-            </CardContent>
-        </Card>
+        <Tabs defaultValue="users" className="w-full">
+            <TabsList className="grid w-full grid-cols-3">
+                <TabsTrigger value="users">Users</TabsTrigger>
+                <TabsTrigger value="products">Products</TabsTrigger>
+                <TabsTrigger value="orders">Orders</TabsTrigger>
+            </TabsList>
+            <TabsContent value="users" className="mt-6">
+                <UsersPage />
+            </TabsContent>
+            <TabsContent value="products" className="mt-6">
+                <ProductsAdminPage />
+            </TabsContent>
+            <TabsContent value="orders" className="mt-6">
+                <OrdersPage />
+            </TabsContent>
+        </Tabs>
     );
 }
